a3b7db2e169091988346e2f1db4b6cc14e8e5a5d
Buhtichka <buhtichka@gmail.com>
2024-05-16 06:10:33 +0300

    Adding new exercise

--- src/chapter5/CalculateCellPhoneBill.java
+++ src/chapter5/CalculateCellPhoneBill.java
@@ -1,4 +1,55 @@
 package chapter5;
 
+
+import java.util.Scanner;
+
+//Calculate the final total of phone bill
+//Input plan fee and number of overage minutes
+//Charge 0.25 for every minute they were over their plan and 15% tax on subtotal
+//Separate methods to calculate tax, overage fee and final total
+//Print result
 public class CalculateCellPhoneBill {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String args[]){
+
+        double planFee = getPlanFee();
+
+        double overageMinutes = getOverageMinutes();
+
+
+        scanner.close();
+
+        double overageFee = getOverageFee(overageMinutes);
+
+        double total = getTotal(planFee, overageFee);
+
+
+    }
+
+    public static double getPlanFee(){
+        System.out.println("Enter your plan fee:");
+        double planFee = scanner.nextDouble();
+        return planFee;
+    }
+
+    public static double getOverageMinutes(){
+        System.out.println("Enter number of overage minutes:");
+        double overageMinutes = scanner.nextDouble();
+        return overageMinutes;
+    }
+
+    public static double getOverageFee(double overageMinutes){
+        double overageFee = overageMinutes*0.25;
+        return overageFee;
+    }
+
+    public static double getTotal(double planFee, double overageFee){
+        double subtotal = planFee+overageFee;
+        double total = subtotal+(subtotal*0.15);
+        System.out.println("The total phone bill is $"+ String.format("%.2f", total));
+        return total;
+    }
+
 }
