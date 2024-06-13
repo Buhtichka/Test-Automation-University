7a7ca602616d0071bd4fd5e7d26410f76d52b292
Buhtichka <buhtichka@gmail.com>
2024-06-13 06:51:33 +0300

    Adding new exercise

---
+++ src/chapter9/InheritanceTester.java
@@ -0,0 +1,11 @@
+package chapter9;
+
+public class InheritanceTester {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.getAge();
+        Employee employee = new Employee();
+        employee.setName();
+    }
+}
